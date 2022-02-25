8be612a9d3abe8f898a387ddd89a58112181a6de
steven1lung <1030steven@gmail.com>
2022-02-26 02:40:32 +0800

    Add github api and simple UI
    Use axios to get github repo data from whom the input entered
    then log out to the console.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [showname, setShowname] = useState("");
+  const [user, setUser] = useState("");
+  const [repos, Setrepos] = useState("");
+
+  const submit = () => {
+    // alert(username);
+
+    axios.get(`https://api.github.com/users/${username}/repos`).then((res) => {
+      setShowname(username);
+      console.log(res.data.length);
+    });
+  };
+
+  const setData = ({}) => {};
+
+  const handlesubmit = (e) => {
+    setUsername(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="navbar">{`Repo Explorer   ${showname}`}</div>
+      <div className="search">
+        <input placeholder="Github User" name="name" onChange={handlesubmit} />
+        <button onClick={submit}>Search</button>
+      </div>
     </div>
   );
 }
-
 export default App;
